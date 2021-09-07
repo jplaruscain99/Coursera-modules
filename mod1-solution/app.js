318876f457b89e8dc32d5f78cde5512300b10ba5
jplaruscain99 <jp.laruscain.99@gmail.com>
2021-09-07 20:23:45 +0800

    Submit updated assignment with bonus features.

--- mod1-solution/app.js
+++ mod1-solution/app.js
@@ -9,21 +9,38 @@ function Controller($scope) {
 
 $scope.lunchlist = "";
 $scope.message = "";
+$scope.messagecolor = "black";
+
 
 $scope.displayMessage = function () {
-  if($scope.lunchlist === ""){
+  var itemlist = $scope.lunchlist;
+  var items =  itemlist.split(',');
+  $scope.messagecolor = "green";
+
+  checkEmpty(items);
+
+  if(items.length == 0){
     $scope.message = "Please enter data first";
+    $scope.messagecolor = "red";
     return;
   }
 
-  var itemlist = $scope.lunchlist;
-  var items =  itemlist.split(',');
-
-  if(items.length > 3)
+  if(items.length > 3){
     $scope.message = "Too much!";
-  else
+  }
+  else{
     $scope.message = "Enjoy!";
+  }
 };
+
+function checkEmpty(list){
+  for(var i=0; i<list.length; i++){
+    if (list[i] === ""){
+      list.splice(i,1);
+      i--;
+    }
+  }
+}
 
 }
 })();
